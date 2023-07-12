2da1c1d2f5259aef6584aecf8b2fcff74c37bc75
ZanGit <marcellopz@hotmail.com>
2023-07-11 23:32:04 -0300

    home border change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import { AuthContext } from "../../contexts/authContext";
 import { getPlayer } from "../../services/firebaseDatabase";
 import { MiscContext } from "../../contexts/miscContext";
 import CardDisplay from "../../common-components/CardDisplay/CardDisplay";
+import X5pageContentArea from "../../common-components/X5pageContentArea";
 
 export default function Home() {
   const { signed, isAnonymous } = useContext(AuthContext);
@@ -20,19 +21,13 @@ export default function Home() {
   }, [signed, getCardbackground]);
 
   return (
-    <div
-      style={{
-        width: "100%",
-        // height: "100vh",
-        paddingTop: "50px",
-      }}
-    >
+    <X5pageContentArea>
       {isAnonymous && (
         <div style={{ marginBottom: "10px" }}>
           To get the full experience log in and request to be a nerd
         </div>
       )}
       <CardDisplay players={players} />
-    </div>
+    </X5pageContentArea>
   );
 }
